bd29f650f1accab0f0703829b761916a62ab40a5
PaxAurora <paxaurora@MacBook-Pro-PaxAurora.local>
2021-10-19 22:47:53 +0200

    Dir check added

--- rename_files_indexing.py
+++ rename_files_indexing.py
@@ -5,39 +5,57 @@ from pydub import AudioSegment
 from scipy.io.wavfile import read
 import logging
 
-audio_formats = ['.mp3', '.flac', '.ogg', '.m4a', '.aiff']
+audio_formats = ['.wav', '.mp3', '.flac', '.ogg', '.m4a', '.aiff']
 
-def index_and_rename(directory):
-    format_check(directory)
+def index_and_rename(database_directory):
     file_index = 0
-    for filepath in pathlib.Path(directory).glob('**/*'):
-        if os.path.isfile:
-            destination = f'{directory}/{str(file_index)}.wav'
+    audiofiles_dir = f'{database_directory}/audio'
+
+    check_directories(database_directory)
+    move_unrecognised(audiofiles_dir)
+    format_check(audiofiles_dir)
+
+    """for filepath in pathlib.Path(audiofiles_dir).glob('**/*'):
+        if os.path.isfile(filepath):
+            
+            destination = f'{audiofiles_dir}/{str(file_index)}.wav'
             os.rename(filepath, destination)
-            file_index += 1
-        else:
-            pass
+            file_index += 1"""
 
 def format_check(path):
     for filepath in pathlib.Path(path).glob('**/*'):
             if os.path.isfile(filepath):
-                if filepath.suffix in audio_formats:
+                if filepath.suffix in audio_formats and filepath.suffix != '.wav':
                     audio_to_wav(filepath)
-                elif filepath.suffix == '.wav':
-                    pass
                 else:
-                    os.rename(filepath, f'{filepath.parents[1]}/unrecognised_audio/{filepath.stem}{filepath.suffix}')
+                    print(filepath)
+                    print(f'{filepath.parents[1]}/unrecognised_files/{filepath.stem}{filepath.suffix}')
                     logging.warning(f'{filepath} extension is not an audio format. Moving to /unrecognised.')
-            else:
-                pass
+                    os.rename(filepath, f'{filepath.parents[1]}/unrecognised_files/{filepath.stem}{filepath.suffix}')
+                    
+                    
 
 def audio_to_wav(filepath):
         audio_to_wav = AudioSegment.from_file(filepath) 
         audio_to_wav.export(f'{filepath.parent}/{filepath.stem}.wav', format="wav")
         os.remove(filepath)
-        
+
+
+def check_directories(directory):
+    if not os.path.exists(f'{directory}/audio'):
+        logging.error("Directory does not contain an 'audio' folder. Check provided path.")
+        exit()
+    elif not os.path.exists(f'{directory}/unrecognised_files'):
+        logging.warning('Path for unrecognised files not found. Folder will be created')
+        os.mkdir(f'{directory}/unrecognised_files')
+
+def move_unrecognised(dir):
+    for filepath in pathlib.Path(dir).glob('**/*'):
+            if filepath.suffix not in audio_formats:
+                destination = f'{filepath.parents[1]}/unrecognised_files/{filepath.stem}{filepath.suffix}'
+                os.rename(filepath, destination)
 
 
 if __name__ == '__main__':
-    path = "/home/maks/Desktop/Dev/Datasets processing/audio"
+    path = "/Users/paxaurora/Desktop/Dev/datasets_processing/AudioDataset"
     index_and_rename(path)
