ba0d3342ffb531a61743568e9bb197b5ff8fa1f7
maks <gutmmm@gmail.com>
2021-10-20 16:12:37 +0200

    processing and indexing

--- rename_files_indexing.py
+++ rename_files_indexing.py
@@ -7,48 +7,44 @@ import logging
 
 audio_formats = ['.wav', '.mp3', '.flac', '.ogg', '.m4a', '.aiff']
 
-def index_and_rename(database_directory):
-    audiofiles_dir = f'{database_directory}/audio'
 
-    check_directories(database_directory)
-    move_unrecognised(audiofiles_dir)
-    format_check(audiofiles_dir)
-    rename_by_index(audiofiles_dir)
+def process(dir):
+    audio_dir = f'{dir}/audio'
+    if not os.path.exists(audio_dir):
+        logging.error("Directory does not contain an 'audio' folder. Check provided path.")
+        exit()
+    elif not os.path.exists(f'{dir}/unrecognised_files'):
+        logging.warning('Path for unrecognised files not found. Folder will be created')
+        os.mkdir(f'{dir}/unrecognised_files')
+    move_unrecognised(audio_dir)
 
-def format_check(path):
-    for filepath in pathlib.Path(path).glob('**/*'):
+def move_unrecognised(audio_dir):
+    for filepath in pathlib.Path(audio_dir).glob('**/*'):
+            if filepath.suffix not in audio_formats:
+                destination = f'{filepath.parents[1]}/unrecognised_files/{filepath.stem}{filepath.suffix}'
+                os.rename(filepath, destination)
+    format_check(audio_dir)
+
+def format_check(audio_dir):
+    for filepath in pathlib.Path(audio_dir).glob('**/*'):
                 if filepath.suffix in audio_formats and filepath.suffix != '.wav':
                     audio_to_wav(filepath)
+    rename_by_index(audio_dir)
+
+def rename_by_index(audio_dir):
+    file_index = 0
+    for filepath in pathlib.Path(audio_dir).glob('**/*'):
+        if os.path.isfile(filepath):
+            destination = f'{audio_dir}/{str(file_index)}.wav'
+            os.rename(filepath, destination)
+            file_index += 1
 
 def audio_to_wav(filepath):
         audio_to_wav = AudioSegment.from_file(filepath) 
         audio_to_wav.export(f'{filepath.parent}/{filepath.stem}.wav', format="wav")
         os.remove(filepath)
 
-def check_directories(directory):
-    if not os.path.exists(f'{directory}/audio'):
-        logging.error("Directory does not contain an 'audio' folder. Check provided path.")
-        exit()
-    elif not os.path.exists(f'{directory}/unrecognised_files'):
-        logging.warning('Path for unrecognised files not found. Folder will be created')
-        os.mkdir(f'{directory}/unrecognised_files')
-
-def move_unrecognised(dir):
-    for filepath in pathlib.Path(dir).glob('**/*'):
-            if filepath.suffix not in audio_formats:
-                destination = f'{filepath.parents[1]}/unrecognised_files/{filepath.stem}{filepath.suffix}'
-                os.rename(filepath, destination)
-
-def rename_by_index(dir):
-    file_index = 0
-    for filepath in pathlib.Path(dir).glob('**/*'):
-        if os.path.isfile(filepath):
-            
-            destination = f'{dir}/{str(file_index)}.wav'
-            os.rename(filepath, destination)
-            file_index += 1
-
 
 if __name__ == '__main__':
     path = "/home/maks/Desktop/Dev/Datasets processing/AudioDataset"
-    index_and_rename(path)
+    process(path)
