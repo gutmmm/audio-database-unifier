536092463021191a81e2781a0b628071fb4ea09b
Aura <gutmmm@gmail.com>
2023-07-05 15:05:48 +0200

    Bugfix in split_audio.py

--- split_audio.py
+++ split_audio.py
@@ -6,16 +6,8 @@ from pathlib import Path
 from pydub import AudioSegment
 from scipy.io.wavfile import read, write
 
-def split():
 
-    prepare_dir()
 
-    for file in os.listdir('AudioExport'):
-        if Path(file).suffix == '.wav':
-            fs, audio = read(os.path.join('AudioExport', file))
-            slice_duration = 4 #in seconds
-            slice_length = fs * slice_duration
-            get_chunks(file, fs, audio, slice_length)
 
 def prepare_dir():
     if "chunks" in os.listdir('AudioExport'):
@@ -26,7 +18,19 @@ def prepare_dir():
 
 def get_chunks(file, fs: int, audio: list, slice_length: int):
     audio = audio[:len(audio) - len(audio)%slice_length]
-    chunks = [audio[slice_length*idx : slice_length*(idx + 1)] for idx in range(6)]
+    chunks = [audio[slice_length*idx : slice_length*(idx + 1)] for idx in range(int(len(audio) / slice_length))]
     for idx, chunk in enumerate(chunks):
-        print(len(chunk))
         write(f'AudioExport/chunks/{idx}_{file}', fs, chunk)
+
+
+def main():
+    prepare_dir()
+    for file in os.listdir('AudioExport'):
+        if Path(file).suffix == '.wav':
+            fs, audio = read(os.path.join('AudioExport', file))
+            slice_duration = 4 #in seconds
+            slice_length = fs * slice_duration
+            get_chunks(file, fs, audio, slice_length)
+
+if __name__ == '__main__':
+    main()
